d36eb5b9affa5dda724dae242b9d7c6bea1f7a71
cumic <changhwan060224@gmail.com>
2024-04-29 18:55:42 +0900

    ScriptableObject

---
+++ BlogGame/Assets/C#/TestScriptableObject.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "testData", menuName = "testData", order = 0)]
+public class TestScriptableObject : ScriptableObject
+{
+    public int hp;
+    public int damage;
+}
